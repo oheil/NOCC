86e4eec6f1e43d7c54f0d32ff78bd4eec9ddc288
oheil <git@heilbit.de>
2023-02-23 14:19:48 +0100

    all syslog calls behind config variable, see discussion https://sourceforge.net/p/nocc/discussion/38301/thread/94712f48c3/

---
+++ webmail/lang/sms.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Language configuration file for NOCC
+ *
+ * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
+ * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
+ *
+ * This file is part of NOCC. NOCC is free software under the terms of the
+ * GNU General Public License. You should have received a copy of the license
+ * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    NOCC
+ * @subpackage Translations
+ * @license    http://www.gnu.org/licenses/ GNU General Public License
+ * @version    SVN: $Id: $
+ */
+/** Skolt Sami (nuõrttsääʹmǩiõll)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Yupik
+ */
+
+$alt_delete_one = 'Jaukkâd saaǥǥ';
+$alt_new_msg = 'Ođđ saaǥǥ';
+$title_prev_page = 'Oouʹdab seidd';
+$html_login = 'Ǩeeʹrjtõõđ sizz';
+$html_user_label = 'Õõʹnni:';
+$html_passwd_label = 'Peittsääʹnn:';
+$html_submit = 'Ǩeeʹrjtõõđ sizz';
+$html_wrong = 'Õõʹnninõmm leʹbe peittsääʹnn lij vââǥǥlaž';
+$html_retry = 'Ǩiččlõõđ oʹđđest';
+$html_lang_label = 'Ǩiõll:';
+$html_preferences = 'Asetõõzz';
+$html_email_address_label = 'E-pååʹštaddrõs:';
+$html_hide_addresses = 'Čiõǥǥ addrõõzzid';
+$html_prefs_updated = 'Asetõõzz lie peiʹvvuum.';
+$html_folders_create_failed = 'Maapp raajjmõš ij oʹnnstam!';
+$html_folder_rename = 'Uuʹd ođđ nõõm';
+$html_folder_delete = 'Jaukkâd';
+$html_filter_remove = 'Jaukkâd';
+$html_filter_name = 'Siʹllʼjeeinõmm:';
+$html_move = 'Seeʹrd';
+$html_copy = 'Kopiââʹst';
+$html_page = 'Seidd';
+$html_reply = 'Vaʹstted';
+$html_delete = 'Jaukkâd';
+$html_new = 'Ođđ';
+$html_mark = 'Jaukkâd';
+$html_att_label = 'Meâlddõs:';
+$html_atts_label = 'Meâlddõõzz:';
+$html_unknown = '[toobdtem]';
+$html_sort = 'Šlajjâd';
+$html_from_label = 'Vuõltteei:';
+$html_date = 'Peiʹvvmieʹrr';
+$html_date_label = 'Peiʹvvmieʹrr:';
+$html_size = 'Šorradvuõtt';
+$html_filename = 'Teâttõsnõmm';
+$html_to_label = 'Vuâsttavaʹlddi:';
+$html_send = 'Vuõlttâd';
+$html_cancel = 'Jõõsk';
+$html_logout = 'Ǩeeʹrjtõõđ åålǥas';
+$html_msg = 'Saakk';
+$html_msgs = 'Saaǥǥ';
+$html_select = 'Vaʹlljed';
+$html_read = 'lokkum';
+$html_add = 'Lââʹzzet';
+$html_contacts = 'Õhttvuõđteâđ – %1$s';
+$html_modify = 'Muuʹtt';
+$html_contact_first = 'Risttnõmm';
+$html_contact_last = 'Sokknõmm';
+$html_contact_mail = 'E-pååʹštaddrõs';
+$html_contact_count = 'Õhttvuõđteâđ %1$d';
+$html_contact_list_add = 'Lââʹzzet liʹstte';
+$html_contact_listname = 'Liist nõmm';
+$html_contact_all = 'Čuäʹjet pukid';
+$html_down_mail = 'Laadd';
+$lang_could_not_connect = 'Serveera õhttummuš ij oʹnnstam';
+$html_invalid_email_address = 'Vââǥǥlaž e-pååʹštaddrõs';
+$html_search = 'Ooʒʒ';
+$html_fd_filename = 'Laadd %1$s';
+$html_fd_filesize = 'šorradvuõtt %1$d';
+$html_version_message3 = 'Ođđ versio lij vuäǯǯamnalla';
