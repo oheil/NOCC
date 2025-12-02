276855d759c82e5a2b13fd92290c4f13248e87a4
oheil <git@heilbit.de>
2025-12-02 17:36:21 +0100

    1.9.15

--- webmail/common.php
+++ webmail/common.php
@@ -11,7 +11,7 @@
  *
  * @package    NOCC
  * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: common.php 3080 2023-03-16 15:35:07Z oheil $
+ * @version    SVN: $Id: common.php 3188 2025-12-02 16:35:43Z oheil $
  */
 
 define('NOCC_DEBUG_LEVEL', 0);
@@ -57,7 +57,7 @@ require_once './utils/crypt.php';
 require_once './utils/translation.php';
 
 $conf->nocc_name = 'NOCC';
-$conf->nocc_version = '1.9.15-dev';
+$conf->nocc_version = '1.9.15';
 $conf->nocc_url = 'http://nocc.sourceforge.net/';
 
 $pwd_to_encrypt = false;
